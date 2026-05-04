93ecf65cf1357c4c4ba8dde9df48f64572986923
Daniil Dotsenko <kamikoto_mono@mail.ru>
2026-05-04 13:22:52 +0300

    Добавлено нормализация изображений продуктов для корректного отображения с CDN

--- apps/web/src/api/products/model.ts
+++ apps/web/src/api/products/model.ts
@@ -16,6 +16,17 @@ export interface IProduct {
   subCategoryName: string
 }
 
+const PRODUCT_IMAGES_ORIGIN =
+  'https://battletoads.ams3.digitaloceanspaces.com'
+
+const PRODUCT_IMAGES_CDN =
+  'https://battletoads.ams3.cdn.digitaloceanspaces.com'
+
+const normalizeProductPhoto = (photo: string) =>
+  photo.startsWith(PRODUCT_IMAGES_ORIGIN)
+    ? `${PRODUCT_IMAGES_CDN}${photo.slice(PRODUCT_IMAGES_ORIGIN.length)}`
+    : photo
+
 export const mapProduct = (dto: IProductDto): IProduct => ({
   brand: dto.name.split(' ')[0] ?? dto.categoryName,
   categoryId: dto.categoryId,
@@ -25,8 +36,8 @@ export const mapProduct = (dto: IProductDto): IProduct => ({
   id: dto.id,
   inStock: dto.inStock,
   name: dto.name,
-  photo: dto.photos[0] ?? null,
-  photos: dto.photos,
+  photo: dto.photos[0] ? normalizeProductPhoto(dto.photos[0]) : null,
+  photos: dto.photos.map(normalizeProductPhoto),
   price: dto.price,
   subCategoryId: dto.subCategoryId,
   subCategoryName: dto.subCategoryName
